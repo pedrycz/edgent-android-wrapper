c8140612284d36613bbf0810a8597f2062db7047
Piotr Wachala <piotr.wachala@outlook.com>
2018-06-18 20:06:14 +0200

    Build fix

--- src/main/java/pl/edu/agh/edgentandroidwrapper/Topology/SimpleTopology.java
+++ src/main/java/pl/edu/agh/edgentandroidwrapper/Topology/SimpleTopology.java
@@ -1,13 +1,11 @@
 package pl.edu.agh.edgentandroidwrapper.Topology;
 
 import android.app.Activity;
-import lombok.Builder;
 import org.apache.edgent.android.hardware.runtime.SensorSourceSetup;
 import org.apache.edgent.providers.direct.DirectProvider;
 import org.apache.edgent.topology.Topology;
 import pl.edu.agh.edgentandroidwrapper.consumer.SensorDataConsumer;
 
-@Builder
 public abstract class SimpleTopology {
     protected String name;
     protected DirectProvider directProvider = new DirectProvider();
